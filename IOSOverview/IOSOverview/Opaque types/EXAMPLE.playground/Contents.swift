e80aae0e3589853216811a686cf30eba4eb719a1
Roma Patel <roma.p@simformsolutions.com>
2022-11-14 11:21:42 +0530

    basic example

--- IOSOverview/IOSOverview/Opaque types/EXAMPLE.playground/Contents.swift
+++ IOSOverview/IOSOverview/Opaque types/EXAMPLE.playground/Contents.swift
@@ -75,3 +75,92 @@ struct Matrix {
 }
 
 print(Matrix(rows: 2, columns: 2))
+
+let greet = { (name: String)  in
+ print(name)
+}
+print(greet)
+
+func greet(name: String) {
+    print(name)
+}
+
+greet(name: "Roma")
+
+let addition = { (a: Int, b: Int) -> Int  in
+
+    return a + b
+}
+
+print(addition(10,20))
+
+class TestA {
+    var completion: ((Bool) -> Void)?
+    func callCompletion() {
+        completion?(true)
+    }
+
+    func setUpCloser(complition: @escaping (Bool)->Void) {
+        self.completion = complition
+    }
+}
+
+let classa = TestA()
+classa.setUpCloser {
+  print("fgndfg\($0)")
+}
+classa.callCompletion()
+
+let numbers = [1, 2 , 3]
+let digitNames = [
+    0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+    5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+]
+let strings = numbers.map {
+    var number = $0
+    var output = ""
+    repeat {
+        output = digitNames[number % 10]! + output
+        number /= 10
+    } while number > 0
+    return output
+}
+
+var helo = "abc"
+var abc2 = { [helo] in
+    print(helo)
+}
+helo = "xyz"
+print(abc2())
+
+enum hello {
+    case east
+    case west
+    case south
+    case north
+    case extra(abc: String)
+    func bhargav() {
+        print("hello")
+    }
+}
+
+hello.east.bhargav()
+hello.extra(abc: "xyz")
+
+
+class RangeOfArray {
+    let array: [Int]
+
+    init(array: [Int]) {
+        self.array = array
+    }
+
+    subscript(from: Int, to: Int) -> [Int] {
+        get {
+            return Array(array[from...to])
+        }
+    }
+}
+let rangeOfArray = RangeOfArray(array: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110])
+print(rangeOfArray[0,4])
+print("")
